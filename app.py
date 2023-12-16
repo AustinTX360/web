5ad211d24366878b0330422268d992d8d2391cf8
AustinTX360 <153311920+AustinTX360@users.noreply.github.com>
2023-12-16 17:08:16 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from FlaskTemplate import app
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT', '8080'))
+        PORT = int(environ.get('SERVER_PORT', '3000'))
     except ValueError:
-        PORT = 8080
+        PORT = 3000
     app.run(HOST, PORT)
